52f4ab497e6a87f0f987ae3a22b5247ddcb0ca60
hiroki.tanida <hiroki.tanida@leverages.jp>
2016-01-23 22:45:24 +0900

    Initial Commit

---
+++ TransitionExample/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  TransitionExample
+//
+//  Created by 谷田　裕樹 on 2016/01/23.
+//  Copyright © 2016年 谷田　裕樹. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
